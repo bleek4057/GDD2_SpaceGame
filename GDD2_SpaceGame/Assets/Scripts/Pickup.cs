f392d0cf87e149d2a12f22abe4f4eaf98fa9ca26
Daniel Lowery <daniel.lowery11@gmail.com>
2016-11-17 11:09:45 -0500

    See if pickup actually goes through

---
+++ GDD2_SpaceGame/Assets/Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+    //the kind of weapon the pickup has
+    GameObject weapon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
