003b43ced6506b478056c951b816023dcd72cedb
Christopher Hipkins <chipkins@csh.rit.edu>
2016-11-28 06:38:48 -0500

    Set up for weapons, create test scene and scripts

--- GDD2_SpaceGame/Assets/Scripts/GameManager.cs
+++ GDD2_SpaceGame/Assets/Scripts/GameManager.cs
@@ -4,16 +4,54 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
     //array for the weapons
-    GameObject weapon;
+    int currentWeapon;
     GameObject[] weapons;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        currentWeapon = 1;
+        weapons = new GameObject[10];
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            currentWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            currentWeapon = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            currentWeapon = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            currentWeapon = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            currentWeapon = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            currentWeapon = 6;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            currentWeapon = 7;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            currentWeapon = 8;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            currentWeapon = 9;
+        }
+    }
 }
