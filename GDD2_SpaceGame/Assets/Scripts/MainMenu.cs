e9a720ae2f7fbeaea6b9d94554f09bf3b309bc65
Daniel Lowery <dtl5161@mail.rit.edu>
2016-12-06 19:14:30 -0500

    Added Maim Menu

---
+++ GDD2_SpaceGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Scenes/WeaponsTest");
+    }
+
+    //
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
